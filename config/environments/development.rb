f9b90ee886c793502a920a576d15a613ae1be70c
Yusuke Kanno <yusukekanno@yusukemacbook.local>
2017-08-12 16:52:59 +0900

    [add]devise,ローカルでのメール認証

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  #devise confirmation mail
+  config.action_mailer.default_url_options = { host: '192.168.99.101', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener_web
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root 'pages#index'
+  get 'pages/show'
+  devise_for :users
   resources :users
+
+  if Rails.env.development? #deviseの確認メール用
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
